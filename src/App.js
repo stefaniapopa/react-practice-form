f8233b61d5b08af995aa53c8eddc68622b4d76c6
Stefania Popa <stefania_florina10@yahoo.com>
2021-08-26 11:35:28 +0300

    did some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Styles } from './Styles';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Basic = () => (
+  <Styles>
+  <div>
+    <h1>Sign Up</h1>
+    <Formik
+      initialValues={{name:'', email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field label='Name' type='name' name='name' placeholder='Enter Name'/>
+          <Field label='Email' type="email" name="email" placeholder="test@test.ro"/>
+          <ErrorMessage name="email" component="div" />
+          <Field label="Password" type="password" name="password" placeholder='Enter Password'/>
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+           </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+  </Styles>
+);
 
-export default App;
+export default Basic;
